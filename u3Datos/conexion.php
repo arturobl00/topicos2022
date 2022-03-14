938be945e47537c5cf4aae016242a40d72549de2
arturobl00 <arturobl00@msn.com>
2022-03-14 09:08:34 -0600

    14/03

---
+++ u3Datos/conexion.php
@@ -0,0 +1,37 @@
+<?php
+    //Archivo de configuración y conexion a un servidor MYSQL
+    //Variables de conexion
+
+    $servername = "localhost";
+    //$servername = "127.0.0.1";
+    $username = "root";
+    $password =  "";
+    $database = "topicosu3";
+
+    //Crear la conexion por MYSQLi Procedural
+    $conn = mysqli_connect($servername,$username,$password,$database);
+
+    if(!$conn){
+        echo("Conexion Fallida".mysqli_connect_error());
+    }
+    else{
+        //echo("Conexion Exitosa");
+        //Primer paso tener la conexion listo
+        //Segundo paso crear un Query (Solicitud al servidor)
+        $querySQL = "Select * From datos";
+        //Tercer Ejecutar el Query en el servidor y retornar datos
+        $result = mysqli_query($conn, $querySQL);
+
+        if(mysqli_num_rows($result) > 0){
+            while($row = mysqli_fetch_assoc($result)){
+                echo "id: ".$row["Id"].
+                " - Nombre: ".$row["Nombre"].
+                " - Apellido: ".$row["Apellido"].
+                " - Edad: ".$row["Edad"]."</br>";
+            }
+        }
+        else{
+            echo "La Tabla no tiene registros";
+        }
+    }
+?>
